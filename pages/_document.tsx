cccf5cee197c3dd515d47520278b333d17928a7f
John Doe <kurbanof.atabek@gmail.com>
2021-11-19 04:20:10 +0600

    created static getInitialProps method for custom document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,29 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-
+import Document, {
+	DocumentContext,
+	DocumentInitialProps,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from 'next/document';
 
 class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-  render() {
-    return (
-      <Html lang="ru">
-        <Head/>
-        <body>
-          <Main/>
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang='ru'>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
-
 
 export default MyDocument;
