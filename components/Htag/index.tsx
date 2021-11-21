525f5cc42396a37253c6da445eb611789bdd55a0
John Doe <kurbanof.atabek@gmail.com>
2021-11-21 17:24:17 +0600

    refactored Htag component for better code read quality

--- components/Htag/index.tsx
+++ components/Htag/index.tsx
@@ -1,6 +1,14 @@
 import { IHtagProps } from './Htag.props';
 
 export const Htag = ({ tag, children }: IHtagProps): JSX.Element => {
-	return <h1>{children}</h1>;
+	switch (tag) {
+		case 'h1':
+			return <h1>{children}</h1>;
+		case 'h2':
+			return <h2>{children}</h2>;
+		case 'h3':
+			return <h3>{children}</h3>;
+		default:
+			return <></>;
+	}
 };
-
