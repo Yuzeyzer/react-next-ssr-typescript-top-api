13ab35e4e9389aac43937b8cc9ef6452257118d4
John Doe <kurbanof.atabek@gmail.com>
2021-11-22 15:52:26 +0600

    add arrow icon, extended Button interface, added some styles for Button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,8 +1,15 @@
 import { IButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
+import ArrowIcon from './arrow.svg';
 
-export const Buton = ({ appearance, children, className, ...props }: IButtonProps): JSX.Element => {
+export const Button = ({
+	appearance,
+	children,
+	arrow = 'none',
+	className,
+	...props
+}: IButtonProps): JSX.Element => {
 	return (
 		<button
 			className={cn(styles.button, className, {
@@ -11,6 +18,14 @@ export const Buton = ({ appearance, children, className, ...props }: IButtonProp
 			})}
 			{...props}>
 			{children}
+			{arrow !== 'none' && (
+				<span
+					className={cn(styles.arrow, {
+						[styles.down]: arrow == 'down',
+					})}>
+					<ArrowIcon />
+				</span>
+			)}
 		</button>
 	);
 };
