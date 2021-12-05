505ade65a69cc62ce2f4d31a98abac8ab232cc47
John Doe <kurbanof.atabek@gmail.com>
2021-12-05 18:45:23 +0600

    created empty Layout FC

---
+++ layout/index.tsx
@@ -0,0 +1,7 @@
+import { ILayoutProps } from './Layout.props';
+import styles from './Layout.module.css';
+import cn from 'classnames';
+
+export const Layout = ({ ...props }: ILayoutProps): JSX.Element => {
+  return <></>;
+};
