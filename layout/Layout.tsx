e5ef9944716aa21702a96f32a4e1eba2810e0aac
John Doe <kurbanof.atabek@gmail.com>
2021-12-05 22:39:55 +0600

    created HoC function <withLayout>

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,8 +4,9 @@ import cn from 'classnames';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Header } from './Header/Header';
 import { Footer } from './Footer/Footer';
+import { FunctionComponent } from 'react';
 
-export const Layout = ({ children }: ILayoutProps): JSX.Element => {
+const Layout = ({ children }: ILayoutProps): JSX.Element => {
   return (
     <>
       <Header />
@@ -16,4 +17,14 @@ export const Layout = ({ children }: ILayoutProps): JSX.Element => {
       <Footer />
     </>
   );
+};
+
+export const withLayout = <T extends Record<string,unknown>>(Component:FunctionComponent<T>) => {
+  return function withLayoutComponent(props:T):JSX.Element {
+    return (
+      <Layout>
+        <Component {...props}/>
+      </Layout>
+    );
+  };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import { Htag, P, Tag, Rating } from '../components';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
-	const [rating, setRating] = useState<number>(3);
-	return (
-		<div>
-			<Htag tag='h1'>Привет мир</Htag>
-			<P size='s'>Текст</P>
-			<P>Текст</P>
-			<P size='l'>Текст</P>
-			<Tag>Ghost</Tag>
-			<Tag color='red'>Red</Tag>
-			<Tag color='green'>Green</Tag>
-			<Rating isEditable rating={rating} setRating={setRating} />
-		</div>
-	);
+function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(3);
+  return (
+    <>
+      <Htag tag='h1'>Привет мир</Htag>
+      <P size='s'>Текст</P>
+      <P>Текст</P>
+      <P size='l'>Текст</P>
+      <Tag>Ghost</Tag>
+      <Tag color='red'>Red</Tag>
+      <Tag color='green'>Green</Tag>
+      <Rating isEditable rating={rating} setRating={setRating} />
+    </>
+  );
 }
+
+export default withLayout(Home)
