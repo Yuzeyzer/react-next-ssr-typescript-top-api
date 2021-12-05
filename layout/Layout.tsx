ff3f847e5e761f3263dbaba5e020560d010c8d59
John Doe <kurbanof.atabek@gmail.com>
2021-12-05 19:00:11 +0600

    created Sidebar and renamed files index.tsx

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ILayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
 import cn from 'classnames';
+import { Sidebar } from './Sidebar/Sidebar';
 
 export const Layout = ({ children }: ILayoutProps): JSX.Element => {
   return (
