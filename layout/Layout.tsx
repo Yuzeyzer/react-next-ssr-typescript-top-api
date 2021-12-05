d732a2985e1044191561288fe8a13dea913e02cb
John Doe <kurbanof.atabek@gmail.com>
2021-12-05 22:26:56 +0600

    created Header and Footer for layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,6 +2,8 @@ import { ILayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
 import cn from 'classnames';
 import { Sidebar } from './Sidebar/Sidebar';
+import { Header } from './Header/Header';
+import { Footer } from './Footer/Footer';
 
 export const Layout = ({ children }: ILayoutProps): JSX.Element => {
   return (
