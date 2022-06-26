8edb2bceb1e59a5e97871977c72d7effe794ecf8
Atabek <kurbanof.atabek@gmail.com>
2022-06-26 19:09:48 +0600

    installed date-fns

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import { IFooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
 import cn from "classnames";
+import { format } from "date-fns";
 
 export const Footer = ({ className, ...props }: IFooterProps): JSX.Element => {
   return (
     <footer className={cn(className, styles.footer)} {...props}>
-      <div>OwlTop © 2020 - 2021 Все права защищены</div>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#" target="_blank" rel="noopener referrer">Пользовательское соглашение</a>
+      <a href="#" target="_blank" rel="noopener referrer">Политика конфиденциальности</a>
     </footer>
   );
 };
