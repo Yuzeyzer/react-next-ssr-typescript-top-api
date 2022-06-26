23d2f79da6d53da5020d9df4a45a1a78713d582f
Atabek <kurbanof.atabek@gmail.com>
2022-06-26 19:05:24 +0600

    add styles for footer layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,11 @@
-import { IFooterProps } from './Footer.props';
-import styles from './Footer.module.css';
-import cn from 'classnames';
+import { IFooterProps } from "./Footer.props";
+import styles from "./Footer.module.css";
+import cn from "classnames";
 
-export const Footer = ({ ...props }: IFooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: IFooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2020 - 2021 Все права защищены</div>
+    </footer>
+  );
 };
